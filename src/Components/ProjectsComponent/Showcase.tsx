152829c9178f30378a2d83e255ff179c970cd914
Railley Nickolei Vince Bautisa <railleynickoleivincebautista@gmail.com>
2025-08-14 15:47:19 +0800

    adding showCase Component

--- src/Components/ProjectsComponent/Showcase.tsx
+++ src/Components/ProjectsComponent/Showcase.tsx
@@ -1,97 +1,88 @@
-const Showcase = () => {
+import { Check } from "lucide-react";
+import { useState } from "react";
+
+export interface ShowcaseData {
+  media: string[];
+  steps: string[];
+  status?: "Complete" | "Ongoing";
+}
+
+const Showcase = ({ media, steps, status = "Complete" }: ShowcaseData) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
   return (
-    <ul className="timeline timeline-vertical">
-      <li>
-        <div className="timeline-middle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-5">
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-              clipRule="evenodd"
+    <div className="flex gap-10 mt-3 bg-base-300 p-4 rounded-lg">
+      <div
+        className="relative p-2  sm:grid bg-gradient-to-b to-blue-950 overflow-hidden justify-center text-left
+          lg:w-[50%] lg:p-0 lg:bg-none flex-1">
+        <div className="grid relative mt-2 rounded-sm overflow-hidden w-full aspect-video lg:mt-0">
+          <div className="flex w-full h-full">
+            {media[selectedIndex].endsWith(".mp4") ? (
+              <video
+                src={media[selectedIndex]}
+                controls
+                autoPlay
+                loop
+                className="object-cover w-full h-full"
+              />
+            ) : (
+              <img
+                src={media[selectedIndex]}
+                alt=""
+                className="object-fill w-200"
+              />
+            )}
+          </div>
+        </div>
+
+        <div className="flex align-center gap-2 p-3 bg-base-100 justify-center shadow-sm rounded-sm mt-3">
+          {media.map((_, index) => (
+            <input
+              key={index}
+              type="radio"
+              name="mediaSwitcher"
+              checked={selectedIndex === index}
+              onChange={() => setSelectedIndex(index)}
+              className=""
             />
-          </svg>
+          ))}
         </div>
-        <div className="timeline-end timeline-box">
-          First Macintosh computer
-        </div>
-        <hr />
-      </li>
-      <li>
-        <hr />
-        <div className="timeline-middle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-5">
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </div>
-        <div className="timeline-end timeline-box">iMac</div>
-        <hr />
-      </li>
-      <li>
-        <hr />
-        <div className="timeline-middle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-5">
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </div>
-        <div className="timeline-end timeline-box">iPod</div>
-        <hr />
-      </li>
-      <li>
-        <hr />
-        <div className="timeline-middle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-5">
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </div>
-        <div className="timeline-end timeline-box">iPhone</div>
-        <hr />
-      </li>
-      <li>
-        <hr />
-        <div className="timeline-middle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-5">
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </div>
-        <div className="timeline-end timeline-box">Apple Watch</div>
-      </li>
-    </ul>
+      </div>
+
+      <ul className="steps steps-vertical flex-1">
+        {steps.map((step, index) => (
+          <li className="step step-primary" key={index}>
+            <span className="step-icon">
+              <Check size={18} />
+            </span>
+            <p className="text-left text-sm">{step}</p>
+          </li>
+        ))}
+
+        <li className="step step-primary">
+          <span className="step-icon">
+            <Check size={18} />
+          </span>
+          <div
+            className={`text-sm text-left flex gap-2 border p-2 rounded-lg  ${
+              status === "Complete"
+                ? "bg-green-400/20 border-green-400"
+                : "bg-yellow-300/40 border-yellow-400"
+            }`}>
+            <div className="inline-grid *:[grid-area:1/1] items-center">
+              <div
+                className={`status  ${
+                  status === "Complete" ? "status-success" : "status-warning"
+                } animate-ping`}></div>
+              <div
+                className={`status ${
+                  status === "Complete" ? "status-success" : "status-warning"
+                }`}></div>
+            </div>
+            {status}
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 };
 
--- src/Pages/ProjectsPage.tsx
+++ src/Pages/ProjectsPage.tsx
@@ -1,7 +1,27 @@
 import Footer from "../Components/Footer";
 import NavLinks from "../Components/NavLinks";
 import ProjectHero from "../Components/ProjectsComponent/ProjectHero";
-import Showcase from "../Components/ProjectsComponent/Showcase";
+import Showcase, {
+  type ShowcaseData,
+} from "../Components/ProjectsComponent/Showcase";
+
+const showcaseProjects: ShowcaseData[] = [
+  {
+    media: [
+      "/img/projects/programming/providence/providence.gif",
+      "/img/projects/programming/providence/providenceLandingPage.jpg",
+      "/img/projects/programming/providence/providencemap.jpeg",
+      "/img/projects/programming/providence/providenceInternmentform.jpeg",
+    ],
+    steps: [
+      "iLocate: An Online Record Management System with Email Notification for Providence Memorial Park-Antipolo",
+      "iLocate is a digital record management system for Providence Memorial Park–Antipolo that allows users to quickly locate the burial lot of a deceased individual and receive timely updates via email notifications.",
+      "This system is developed as a capstone project for the academic year 2024–2025.",
+      "This system is developed as a capstone project for the academic year 2024–2025.",
+    ],
+    status: "Complete",
+  },
+];
 
 const Projects = () => {
   return (
@@ -10,10 +30,12 @@ const Projects = () => {
       <div className="flex justify-center h-auto ">
         <div className="w-7xl h-auto  flex flex-col">
           <ProjectHero />
-          <Showcase />
+          {showcaseProjects.map((project, index) => (
+            <Showcase key={index} {...project} />
+          ))}
         </div>
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
