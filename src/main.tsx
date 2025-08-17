1d3fe16ffa8e6fdbf0c8aaa3ba0d7bce1f514cf6
Railley Nickolei Vince Bautisa <railleynickoleivincebautista@gmail.com>
2025-08-18 04:16:21 +0800

    adjustmentssssd

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router";
+
 import Homepage from "./Pages/Homepage.tsx";
 import Projects from "./Pages/ProjectsPage.tsx";
 import Editing from "./Pages/EditingPage.tsx";
 import About from "./Pages/AboutPage.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Homepage />,
-  },
-  {
-    path: "/programming",
-    element: <Projects />,
-  },
-  {
-    path: "/editing",
-    element: <Editing />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-]);
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/programming" element={<Projects />} />
+        <Route path="/editing" element={<Editing />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>
 );
