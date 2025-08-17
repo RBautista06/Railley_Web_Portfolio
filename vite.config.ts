ebbd80ac3a542a16d9136f2ee312fc8685586fa2
Railley Nickolei Vince Bautisa <railleynickoleivincebautista@gmail.com>
2025-08-18 04:33:32 +0800

    no base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Railley_Web_Portfolio/",
   plugins: [react(), tailwindcss()],
 });
